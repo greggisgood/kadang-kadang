a4d818c1a158c6df305d7347e456b91205ea0389
Lianne Lim <lkrlimrian@gmail.com>
2018-08-16 18:16:04 +0800

    splash screen

---
+++ Assets/Scripts/loadSplash.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class loadSplash : MonoBehaviour {
+
+	public Texture imageToDisplay;
+	public float timeToDisplayImage;
+	public string nextLevelToLoad;
+
+	private float timeForNextLevel;
+
+	// Use this for initialization
+	void Start () {
+		timeForNextLevel = Time.time + timeToDisplayImage;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnGUI() {
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), imageToDisplay);
+		if (Time.time >= timeForNextLevel) {
+			Application.LoadLevel(nextLevelToLoad	);
+		}
+	}
+}
